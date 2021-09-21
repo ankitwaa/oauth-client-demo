921e83593ceb208156c98c1bffee369037a1737f
ankit <ankitforstudy@gmail.com>
2021-09-21 22:12:08 +0530

    github auth commit using grant type code

--- src/main/java/com/example/oauthclientdemo/controller/OAuthDemoController.java
+++ src/main/java/com/example/oauthclientdemo/controller/OAuthDemoController.java
@@ -1,5 +1,7 @@
 package com.example.oauthclientdemo.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuthDemoController {
 
     @GetMapping("/")
-    public String hell(){
+    public String hell(OAuth2AuthenticationToken token){
+        // We can logged in user details using - OAuth2AuthenticationToken
+        System.out.println(token.getPrincipal());
         return "main.html";
     }
 }
